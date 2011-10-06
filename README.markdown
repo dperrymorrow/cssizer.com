f9fb5923b616ee7c476a7b0902714972eb08bca1
dperrymorrow <dperrymorrow@gmail.com>
2011-10-05 22:42:53 -0600

    better readme

--- README.markdown
+++ README.markdown
@@ -1,2 +1,20 @@
 # Cssizer.com
 ## A real-time online CSS editing tool built in CodeIgniter
+
+### Installation
+
+- you need to setup a config/database.php file, there is an example there...
+- you need to run the latest sql file located in config/mysql
+- you need to add your localhost env to the develop switch in index.php
+
+i think you will need to run as root, so setup a vhost alias with MAMP or whatever you prefer
+    
+    switch ( $_SERVER[ 'HTTP_HOST' ] ) {
+	    case 'cssizer.local:8888':
+		    define('ENVIRONMENT', 'development');
+		  break;
+	
+	    default:
+		    define('ENVIRONMENT', 'production');
+		  break;
+    }
--- application/config/config.php
+++ application/config/config.php
@@ -244,15 +244,15 @@ $config['encryption_key'] = 'cgFLirRAb4A4UfFiD3LzeKzQjNL4ZTvc';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_cookie_name']		= 'cssizer_session';
+$config['sess_expiration']		= 72000000;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 300;
+$config['sess_time_to_update']	= 3000;
 
 /*
 |--------------------------------------------------------------------------
