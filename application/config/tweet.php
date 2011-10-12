63b56515a656dde02ef56605cf794883c6e9bfcc
David Morrow <dperrymorrow@gmail.com>
2011-10-12 11:14:40 -0600

    security fix

--- application/config/tweet.php
+++ application/config/tweet.php
@@ -1,4 +1,4 @@
 <?php
 
-	$config['tweet_consumer_key'] = "Q6HSJxdFfq6eUQGXao3maw";
-	$config['tweet_consumer_secret'] = "W4fT9e0C1zqMpqpx77NF1HcHefVlX46y8WaCusLaaI";
+	$config['tweet_consumer_key'] = "XXXX";
+	$config['tweet_consumer_secret'] = "XXXX";
